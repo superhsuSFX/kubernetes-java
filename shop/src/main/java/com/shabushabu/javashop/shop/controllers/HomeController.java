4a4fea9e150110d02b17b0ab6df5b944cd8652e8
shabuhabs <32849847+shabuhabs@users.noreply.github.com>
2021-01-07 12:27:18 -0700

    Update HomeController.java

--- shop/src/main/java/com/shabushabu/javashop/shop/controllers/HomeController.java
+++ shop/src/main/java/com/shabushabu/javashop/shop/controllers/HomeController.java
@@ -21,7 +21,6 @@ public class HomeController {
     @Autowired
     private ProductService productService;
 
- //  @RequestMapping("/") 
    @RequestMapping(value="/", method = RequestMethod.GET)
    public String usingRequestParam(Model model, @RequestParam(value="name", required=true) String thename, @RequestParam(value="color", required=true) String thecolor) {
      
@@ -30,85 +29,30 @@ public class HomeController {
      try (Scope scope = s_tracer.scopeManager().activate(span)) {
          span.setTag("name",thename);
          span.setTag("favcolor", thecolor);
-         model.addAttribute("user", new User());
-         model.addAttribute("products", productService.getProducts());
-		   	     
+	 	// ORIGINAL CODE
+         	model.addAttribute("user", new User());
+        	 model.addAttribute("products", productService.getProducts());
+	 	// END ORIGINAL CODE	   	     
          } finally {
            span.finish();
 	 }		       
      return "index";
-   }
-   
-   
+   } 
     @PostMapping("/adduser")
     public String addUser(@ModelAttribute User user, Model model) {
     	 final Tracer s_tracer = GlobalTracer.get();
          final Span span = s_tracer.buildSpan("adduser").start();
          try (Scope scope = s_tracer.scopeManager().activate(span)) {
+	     
              span.setTag("name", user.getName());
              span.setTag("favcolor", user.getColor());
-             model.addAttribute("products", productService.getProducts()); 	     
+             // ORIGINAL CODE
+	     model.addAttribute("products", productService.getProducts()); 	
+	     // END ORIGINAL CODE
          } finally {
                span.finish();
          }		       
          return "index";
     }
- /*  
-   @RequestMapping(value="/adduser", method = RequestMethod.POST)
-   public String setUser(Model model,  @RequestParam String name, @RequestParam String color) {
-
-       final Tracer s_tracer = GlobalTracer.get();
-       final Span span = s_tracer.buildSpan("adduser").start();
-       try (Scope scope = s_tracer.scopeManager().activate(span)) {
-           span.setTag("name", name);
-           span.setTag("favcolor", color);
-           model.addAttribute("products", productService.getProducts());
-  		   	     
-           } finally {
-             span.finish();
-           }		       
-       return "index";
-   }
-*/
-   
-  /* 
-   @RequestMapping("/")
-   public String index(Model model) {
-	model.addAttribute("products", productService.getProducts());
-        return "index";
-    }
-  */
-/*    
-    @GetMapping("/register")
-    public String showForm(Model model) {
-        User user = new User();
-        model.addAttribute("user", user);
-	return "register_form";
-    }
-	
-    @PostMapping("/register")
-        public String submitForm(@ModelAttribute("user") User user) {
-		//System.out.println(user);
-	    return "redirect:register/success";
-    }
-
-    @RequestMapping("/success")
-    public String index(Model model) {
-        model.addAttribute("products", productService.getProducts());
-        return "product_index";
-    }
- */
-  /*  @GetMapping("/greeting")
-    public String greetingForm(Model model) {
-      model.addAttribute("greeting", new Greeting());
-    return "greeting";
-    }
-
-    @PostMapping("/greeting")
-    public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
-      model.addAttribute("greeting", greeting);
-      return "result";
-   }
-   */
-	
+ 
 }
